a9215499b8d975042aedd4e5907f1c80994b5f9a
Aquila Afuadajo <aquilaafuadajo@gmail.com>
2020-04-04 19:06:10 +0100

    set up authentication middleware to check if user is a valid user before they can post screams

--- functions/index.js
+++ functions/index.js
@@ -42,10 +42,38 @@ app.get('/screams', (req, res) => {
 //   response.send("Hello from Firebase!");
 // });
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No token found')
+    return res.status(403).json({error: 'Unauthorized'});
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log({decodedToken: decodedToken})
+      return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token', err)
+      return res.status(403).json(err)
+    })
+}
+
+app.post('/scream', FBAuth, (req, res) => {
   const newScream = {
     body: req.body.body, 
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
